79e833aba3b6e6d2de083cd4c83ef09b08e1ee08
Kevin Marcillo <168557666+kgmarcillo@users.noreply.github.com>
2024-07-02 22:51:58 -0500

    Apply API
    Applying API to the weather web

---
+++ Home.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+import { useState } from "react";
+import "./Styles/Style.css";
+import { useEffect } from "react";
+
+function Home() {
+  const [data, setData] = useState({
+    celcius: 10,
+    name: "London",
+    humidity: 10,
+    speed: 2,
+  });
+
+async function obtenerDatos(){
+  try {
+    let respuesta = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=Germany&appid=eef165ef40f4fe4ecd97c931861f1a74&&units=metric`)
+    let datos = await respuesta.json();
+    return datos;
+  } catch (error){
+    console.error('Error:', error);
+  }
+}
+
+useEffect(() => {
+  obtenerDatos().then((res) => {
+    setData({
+      celcius: res.main.temp,
+      name: res.name,
+      humidity: res.main.humidity,
+      speed: res.wind.speed,
+    });
+  });
+}, []);
+
+  return (
+    <div className="container">
+      <div className="weather">
+        <div className="search">
+          <input type="text" placeholder="Enter City Name"/>
+          <button>
+            <img src="/images/search.png" alt="" />
+          </button>
+        </div>
+        <div className="winfo">
+          <img className="icon" src="/images/cloud.png" alt="" />
+          <h2>{Math.round(data.celcius)}°c</h2>
+          <h2>{data.name}</h2>
+          <div className="details">
+            <div className="col">
+              <img src="/images/humidity.png" alt="" />
+              <div className="humidity">
+                <p>{Math.round(data.humidity)}%</p>
+                <p>humidity</p>
+              </div>
+            </div>
+            <div className="col">
+              <img src="/images/wind.png" alt="" />
+              <div className="wind">
+                <p>{Math.round(data.speed)}km/h</p>
+                <p>wind</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+
+export default Home;
